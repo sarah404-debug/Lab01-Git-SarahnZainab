17602a3c0aa8c45092d0bcfdab8c934e08d2c4f0
Sarah Faisal <sarahfaisal055@gmail.com>
2026-01-29 01:57:46 +0500

    lab 2 changes

--- Lab02/task4.s
+++ Lab02/task4.s
@@ -2,34 +2,46 @@
 .globl main
 
 main:
-    li x5, 5            # a = 5
-    li x6, 4            # b = 4
-    li x10, 0x200       # base address of D
-    
-    li x7, 0            # i = 0
+    li x5, 3          # a (outer loop limit)
+    li x6, 4          # b (inner loop limit)
+    li x7, 0          # i counter
+    li x10, 0x200     # base address of array D
 
-outerloop:
-    bge x7, x5, end     # if i >= a, exit
-    li x29, 0           # j = 0
+outer_loop:
+    bge x7, x5, end_loop
+    li x29, 0         # reset j for each i
 
-innerloop:
-    bge x29, x6, nextI  # if j >= b, increment i
-    # address calculation for D[4*j]
-    # each word is 4 bytes, so D[4*j] is at offset 16*j
-    slli t0, x29, 4     # t0 = j * 16 (shift left 4)
-    add t1, x10, t0     # t1 = address of D[4*j]
-    
-    # logic: D[4*j] = i + j
-    add t2, x7, x29     # t2 = i + j
-    sw t2, 0(t1)        # store i+j in memory
-    
-    addi x29, x29, 1    # j++
-    j innerloop         # jump to start of inner loop
+inner_loop:
+    bge x29, x6, inc_i
 
-nextI:
-    addi x7, x7, 1      # i++
-    j outerloop         # jump to start of outer loop
+    li x11, 4
+    mul x11, x11, x29     # offset = 4 * j
+    add x11, x11, x10     # base + offset
+
+    add x12, x7, x29      # value = i + j
+    sw  x12, 0(x11)
+
+    addi x29, x29, 1
+    j inner_loop
+
+inc_i:
+    addi x7, x7, 1
+    j outer_loop
+
+end_loop:
+    j end
 
 end:
-    j end          
+    j end
 
+
+
+
+
+
+
+
+
+
+
+    
